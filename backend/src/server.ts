1f7ef3ec440db804d1dfe6db7e26d7860830ffca
K0W4 <kowaleskigabriel@gmail.com>
2024-09-30 16:42:31 -0300

    Finalizando modelo da API

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,14 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors"
 import { routes } from "./routes";
+import { error } from "console";
+import { request } from "http";
 
 const app = Fastify({ logger: true})
+
+app.setErrorHandler((error, request, reply) => {
+    reply.code(400).send({ message: error.message })
+})
 
 const start = async () => {
     await app.register(cors);
