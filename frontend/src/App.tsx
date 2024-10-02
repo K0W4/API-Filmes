725496410a7bead9c64f189486dcfed39badd1d0
K0W4 <kowaleskigabriel@gmail.com>
2024-10-02 20:25:42 -0300

    Adaptação do frontend para atender aos requisitos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,82 +2,92 @@ import { useEffect, useState, useRef, FormEvent } from 'react'
 import { FiTrash } from 'react-icons/fi'
 import { api } from './services/api'
 
-interface CustomerProps
+interface MovieProps
 {
   id: string;
   name: string;
-  email: string;
-  status: boolean;
-  creat_at: string;
+  evaluation: string;
+  rating: string;
 }
 
 export default function App()
 {
-  const [customers, setCustomers] = useState<CustomerProps[]>([])
+  const [movies, setMovies] = useState<MovieProps[]>([])
   const nameRef = useRef<HTMLInputElement | null>(null)
-  const emailRef = useRef<HTMLInputElement | null>(null)
+  const evaluationRef = useRef<HTMLInputElement | null>(null)
+  const ratingRef = useRef<HTMLInputElement | null>(null)
 
   useEffect(() => {
-    loadCustomers();
+    loadMovies();
   }, [])
 
-  async function loadCustomers()
+  async function loadMovies()
   {
-    const response = await api.get("/customers")
-    setCustomers(response.data)
+    const response = await api.get("/movies")
+    setMovies(response.data)
   }
 
   async function handleSubmit(event: FormEvent)
   {
     event.preventDefault();
 
-    if(!nameRef.current?.value || !emailRef.current?.value) return;
+    if(!nameRef.current?.value || !evaluationRef.current?.value || !ratingRef.current?.value) return;
 
-    const response = await api.post("/customer",
+    const response = await api.post("/movie",
     {
       name: nameRef.current?.value,
-      email: emailRef.current?.value
+      evaluation: evaluationRef.current?.value,
+      rating: ratingRef.current?.value
     })
 
-    setCustomers(allCustomers => [...allCustomers, response.data])
+    setMovies(allMovies => [...allMovies, response.data])
 
     nameRef.current.value = ""
-    emailRef.current.value = ""
+    evaluationRef.current.value = ""
+    ratingRef.current.value = ""
   }
 
   async function handleDelet(id: string)
   {
-    await api.delete("/customer", {
+    await api.delete("/movie", {
       params:
       {
         id: id
       }
     })
 
-    const allCustomers = customers.filter( (customer) => customer.id !== id)
-    setCustomers(allCustomers)
+    const allMovies = movies.filter( (movie) => movie.id !== id)
+    setMovies(allMovies)
   }
 
   return(
     <div className="w-full min-h-screen bg-gray-900 flex justify-center px-4">
       <main className="my-10 w-full md:max-w-2xl">
-        <h1 className="text-4xl font-medium text-white flex justify-center">Clientes</h1>
+        <h1 className="text-4xl font-medium text-white flex justify-center">Resenhas</h1>
 
         <form className="flex flex-col my-6" onSubmit={handleSubmit}>
           <label className="font-medium text-white">Nome:</label>
           <input
           type="text" 
-          placeholder="Digite seu nome completo..."
+          placeholder="Digite o nome do filme..."
           className="w-full mb-5 p-2 rounded"
           ref={nameRef}
           />
 
-          <label className="font-medium text-white">Email:</label>
+          <label className="font-medium text-white">Avaliação:</label>
           <input
           type="text" 
-          placeholder="Digite seu email..."
+          placeholder="Digite sua avaliação..."
           className="w-full mb-5 p-2 rounded"
-          ref={emailRef}
+          ref={evaluationRef}
+          />
+
+          <label className="font-medium text-white">Nota:</label>
+          <input
+          type="text" 
+          placeholder="Digite sua nota..."
+          className="w-full mb-5 p-2 rounded"
+          ref={ratingRef}
           />
 
           <input
@@ -87,18 +97,18 @@ export default function App()
         </form>
 
         <section className="flex flex-col gap-4">
-          {customers.map( (customer) => (
+          {movies.map( (movie) => (
           <article
-            key={customer.id}
+            key={movie.id}
             className="w-full bg-white rounded p-2 relative hover:scale-105 duration-200"
           >
-            <p><span className="font-medium">Nome:</span> {customer.name} </p>
-            <p><span className="font-medium">Email:</span> {customer.email} </p>
-            <p><span className="font-medium">Status:</span> {customer.status ? "ATIVO" : "INATIVO"} </p>
+            <p><span className="font-medium">Nome:</span> {movie.name} </p>
+            <p><span className="font-medium">Avaliação:</span> {movie.evaluation} </p>
+            <p><span className="font-medium">Nota:</span> {movie.rating} </p>
 
             <button
             className="bg-red-500 w-7 h-7 flex items-center justify-center rounded-lg absolute right-0 -top-2"
-            onClick={ () => handleDelet(customer.id)}
+            onClick={ () => handleDelet(movie.id)}
             >
               <FiTrash size={18} color='#FFF'/>
             </button>
